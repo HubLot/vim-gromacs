00e4194ac211c930702941fc1ea2812d5804a809
Jonathan Barnoud <jonathan@barnoud.net>
2012-12-10 22:43:09 +0100

    Minor updates

--- autoload/mdp_complete.py
+++ autoload/mdp_complete.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# A copy of the GNU General Public License is available at
+# http://www.gnu.org/licenses/gpl-3.0.html.
 
 """
-This is a prototype for completeion suggestion in the context of a GROMACS mdp
-file. The final purpose is to propose autocompletetion for mdp files in vim.
+Contextual completion for mdp files.
 """
+
+__author__ = 'Jonathan Barnoud'
 
 import vim
 
